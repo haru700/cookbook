57f8e12ab2382b36bf86cdc5ae1ca291ed145cea
HARUHITO YAMAZAKI <haru@HARUHITOnoiMac.local>
2017-05-14 02:01:26 +0900

    node.json, roles/setup.json, soro.rb, templates/default/virtualhost.conf.erb 新規作成 recipes/default.rb記述

--- kbtitCookbbok/recipes/default.rb
+++ kbtitCookbbok/recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+pakage "httpd"
+
+template "/etc/httpd/conf.d/#{ENV['HOSTNAME']}.conf" do
+  source "virtualhost.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  variables({
+    :env_hostname => "#{ENV['HOSTNAME']}",
+  })
+end
+
+directory "#{node[:apache][:documentroot]/ENV['HOSTNAME']}" do
+  action :create
+end
+
+git "/var/www/#{ENV['HOSTNAME']}" do
+  repository "https://github.om/haru700/chefKbtit.git"
+  reference "master"
+  action :sync
+end
+
+service "httpd" do
+  action :start
+end
